32f8526af775882025eca1fff601cfdbdb57b85c
Michael <michael.glaude@gmail.com>
2014-07-11 13:26:38 -0400

    Remove many print statements and comments. Also removed some reduntant and uneeded statements.

--- experiments/OCDS_Api.py
+++ experiments/OCDS_Api.py
@@ -14,63 +14,6 @@ import simplejson
 import json
 
 #----------------------------------------------------------------------------
-#Examples: of server http or https urls
-
-#Getting all links one page at a time...
-#ocds_api_url = 'http://ocds.open-contracting.org/opendatacomparison/api/links'
-
-#Getting all links one page at a time with page size set...for Pagination ...
-#ocds_api_url = 'http://ocds.open-contracting.org/opendatacomparison/api/links?page=5&page_size=5'
-
-#Getting only csv links...Sarah configure Django server page size to 500...
-#ocds_api_url = 'http://ocds.open-contracting.org/opendatacomparison/api/links/f/csv?format=json' 
-#----------------------------------------------------------------------------
-"""Example: of data structure returned by server...with 5 results all CSVs...
-
-{
-    "count": 71,
-    "next": "http://ocds.open-contracting.org/opendatacomparison/api/links/f/csv?page=2&page_size=5",
-    "previous": null,
-    "results": [
-        {
-            "dataset": "Contracts Finder - All Notices",
-            "format": "CSV",
-            "link": "http://ocds.open-contracting.org/opendatacomparison/download/114/",
-            "notes": "",
-            "title": "Older Notices - January 2012"
-        },
-        {
-            "dataset": "Buyandsell.gc.ca - Tenders Data",
-            "format": "CSV",
-            "link": "http://ocds.open-contracting.org/opendatacomparison/download/11/",
-            "notes": "microsoft CSV vs CSV",
-            "title": "New today tender notices"
-        },
-        {
-            "dataset": "Contracts Finder - All Notices",
-            "format": "CSV",
-            "link": "http://ocds.open-contracting.org/opendatacomparison/download/80/",
-            "notes": "",
-            "title": "Older Notices - June 2013"
-        },
-        {
-            "dataset": "OpenTED - Contract Awards",
-            "format": "CSV",
-            "link": "http://ocds.open-contracting.org/opendatacomparison/download/215/",
-            "notes": "",
-            "title": "Contract Awards - 2012"
-        },
-        {
-            "dataset": "Buyandsell.gc.ca - Tenders Data",
-            "format": "CSV",
-            "link": "http://ocds.open-contracting.org/opendatacomparison/download/18/",
-            "notes": "",
-            "title": "Construction notices"
-        }
-    ]
-}
-"""
-#----------------------------------------------------------------------------
 def get_json_dict(ocds_api_url='http://ocds.open-contracting.org/opendatacomparison/api/links?'):
     """Gets metadata from opendatacomparison server"""        
     try:
@@ -98,26 +41,19 @@ def get_json_dict(ocds_api_url='http://ocds.open-contracting.org/opendatacompari
     except Exception, e:
         print "failed at load"
         raise(e)
-    #Future features:
-    # - print 'informative message for failing'
-    # - handle more specific exceptions
     return json_dict
 #----------------------------------------------------------------------------
 
 def get_json_dict_select(num=5, ocds_api_url='http://ocds.open-contracting.org/opendatacomparison/api/links/f/csv?'):
-    
-    ocds_api_url+='page=1&page_size='+str(num) #...   page=1&page_size=5
+    """Gets the specified number on links from the server"""
+    ocds_api_url+='page=1&page_size='+str(num) #...page=1&page_size=5
     json_dict = get_json_dict(ocds_api_url)
-    #Future Features:
-    # - Investigate what queries the server can handle and implement select options. 
-    # - Pagination?
     return json_dict
 #----------------------------------------------------------------------------
 
 def print_json_dict(json_dict):
+    """Provides simple interface to printing json_dict data structure"""
     print json.dumps(json_dict, sort_keys=True, indent=4, separators=(',', ': '))
-    #Future Feature:
-    # - print 3 results by default
     return
 #==============================================================================  
 
@@ -138,10 +74,12 @@ def get_links(json_dict, key='', value='',prn=True): #, key='format', value='CSV
 #-------------------------------------------------------------------------
 
 def get_all_links(json_dict):
+    """Returns all url links"""
     return get_links(json_dict)
 #-------------------------------------------------------------------------
 
 def get_all_csv_links(json_dict):
+    """Returns all url links that have CSV file format"""
     return get_links(json_dict,'format','CSV')
 #==============================================================================   
 
@@ -150,12 +88,6 @@ def get_all_csv_links(json_dict):
 def get_filename_from_url(url):
     """Rudamentary parsing of URL into a list and assumming last element is the filename """
     filename = url.split('/')[-1]
-    #Futur Feature:
-    # - Check for valid filname
-    # - must handle this case:  and probably several more.
-    #   (31536000, 'https://online.contractsfinder.businesslink.gov.uk:443/PublicFileDownloadHandler.ashx?fileName=notices_2012_05.csv&recordType=Notices&fileContent=Monthly', 'PublicFileDownloadHandler.ashx?fileName=notices_2012_05.csv&recordType=Notices&fileContent=Monthly')
-    # - make functions to handle different cases like...def get_filename_from_content-disposition
-    # - run on all data and handle all cases.
     return filename 
 #------------------------------------------------------------------------------
 
@@ -194,49 +126,12 @@ def get_file_info(link, prn=True):
     #if chk_filename  #xxx ...implement later.
     
     the_file.close()
-    #Future Features:
-    # - verify that the filesize is correct. Can be done once downloaded.
-    # - add expire date
-    # - add try except
-    # - check url & filename
     return httpInstance.dict,(fileSize, url, filename )  #...should return struct, filename etc..
 #------------------------------------------------------------------------------
 
 def get_all_files_info(json_dict):
-    """ToDo:  Get info on all files i.e. extend/augment json_dict and return
-    augmented data structure to include added info/metadata about all files.
- 
-    Purpose:  to allow users to select downloading files based on metadata.
-
-    Warnings:
-    1) filename extration is currently rudimentry. Please verify.
-       Upgrade soon to come.
-    2) FILE_SIZE_FAILED = '999999999999' or interger 999999999999
-
-    Example: of augmented resutl withing json_dict
-        Note: file_size, filename, http_dict and url have be added...
-            {
-            "dataset": "Buyandsell.gc.ca - Contract History",
-            "file_size": 24360369,
-            "filename": "tpsgc-pwgsc_co-ch_EF-FY-13-14.csv",
-            "format": "CSV",
-            "http_dict": {
-                "accept-ranges": "bytes",
-                "connection": "close",
-                "content-length": "24360369",
-                "content-type": "application/octet-stream",
-                "date": "Fri, 04 Jul 2014 16:42:38 GMT",
-                "last-modified": "Sun, 01 Jun 2014 14:52:41 GMT",
-                "server": "nginx",
-                "strict-transport-security": "max-age=31536000",
-                "x-frame-options": "SAMEORIGIN"
-            },
-            "link": "http://ocds.open-contracting.org/opendatacomparison/download/5/",
-            "notes": "",
-            "title": "Contract History - 2013-2014",
-            "url": "https://buyandsell.gc.ca/cds/public/contracts/tpsgc-pwgsc_co-ch_EF-FY-13-14.csv"
-        }
-    """    
+    """Gets info on all files i.e. extend/augment json_dict and return
+    augmented data structure to include added info/metadata about all files."""
     results = json_dict['results']
     for i,res in enumerate(results):
         http_dict, file_info =  get_file_info(res['link'])
@@ -248,10 +143,6 @@ def get_all_files_info(json_dict):
         res['filename'] = filename
         results[i]= res
     json_dict['results']=results
-    #Future Features:
-    # - caching:check if already downloaded, must include expery date check but can
-    #   also start by checking by filename &filesize.
-    # -
     return json_dict
 
 #==============================================================================
@@ -262,8 +153,6 @@ def save_file(url, simulate=False):
    #getting the file contents...    
    the_file = urllib2.urlopen(url)
    url_redirect = the_file.url  #...note1: redirect from Django, Note2: same as the_file.geturl()
-   #Parse theUrl to get filename.
-   file_name = get_filename_from_url(url_redirect) 
 
    file_info_dict,(file_size, url_redirect, file_name ) = get_file_info(url,False)
    
@@ -283,27 +172,12 @@ def save_file(url, simulate=False):
            print "Simulation in progress: creating empty file in current directory"
        f.close()
    the_file.close()
-   #Future Features:
-   # - maybe call this "download_file" from url
-   #-  add path to local files
-   # - check if file has been downloaded ( file size, chksum etc)
-   # - return message regarding successful transfer, overwitten etc.
-   # - and try, except and handle specific conditions.
-   # - handle multiple downloads at higher level
-   # - verbose option
-   # - simulate download first option
-   # - estimate time to download
-   # - progress of download
-   # - can easily log or put into Dict or sqlight
    return 
 #-----------------------------------------------------------------------------    
 def save_files(links, simulate=False):
     """Downloads and saves files to local drive at current directory."""
     for link in links:
         save_file(link, simulate)
-    #Future Features:
-    # - add path to local drive
-    # - maybe rename to download_files
     return
 #-----------------------------------------------------------------------------    
 
@@ -312,13 +186,9 @@ def save_csvs_by_dataset(value="Contracts Finder - All Notices",num=5, simulate=
     a specified dataset value.""" 
     json_dict_all_csvs = get_json_dict('http://ocds.open-contracting.org/opendatacomparison/api/links/f/csv?')
 
-    print_json_dict(json_dict_all_csvs)
     links = get_links(json_dict_all_csvs, 'dataset', value ) #...selecting links
-    print "links:  ", links
+
     save_files(links, simulate)
-    #Future Feature:
-    # - value should be searched case insensitive.
-    # - value contains "all notices"
     return 
 #-----------------------------------------------------------------------------    
 
@@ -330,25 +200,22 @@ def save_csvs_limited_by_size(num=5, size=1000):
     return    
 #-----------------------------------------------------------------------------    
 
-
-
 def use_cases():
+    """Provide user some example use cases"""
     FILE_SIZE_FAILED = '999999999999'
     simulate = True
 
     #Examples: Use cases...
-    json_dict_all      = get_json_dict()  #...gets all links from ocds api.
+    #json_dict_all      = get_json_dict()  #...gets all links from ocds api.
     
-    json_dict_all_csvs = get_json_dict('http://ocds.open-contracting.org/opendatacomparison/api/links/f/csv?')
+    #json_dict_all_csvs = get_json_dict('http://ocds.open-contracting.org/opendatacomparison/api/links/f/csv?')
     
     json_dict_5_csvs   = get_json_dict_select()  #...gets up to 5 csv links from ocds api.
     
-    json_dict_500_csvs = get_json_dict_select(500) #...gets up to 500 csv links
+    #json_dict_500_csvs = get_json_dict_select(500) #...gets up to 500 csv links
     
-    json_dict_20       = get_json_dict_select(20, 'http://ocds.open-contracting.org/opendatacomparison/api/links?')
+    #json_dict_20       = get_json_dict_select(20, 'http://ocds.open-contracting.org/opendatacomparison/api/links?')
     
-    print_json_dict(json_dict_5_csvs)
-
     json_dict = json_dict_5_csvs
     #==========================================================================
    
@@ -361,21 +228,16 @@ def use_cases():
     #==========================================================================
     #gathering more informatinon regarding one file...
     http_info, file_info = get_file_info(links2[0])  #...gets augmented info regarding one file
-    print_json_dict(http_info) #... print http information collected from server
-    print_json_dict(file_info) #...prints file size, url & filename
     #getting information of all files and augmenting json_dict...
     json_dict_augmented = get_all_files_info(json_dict)  #...metadata all collected in one place.
-    print_json_dict(json_dict_augmented)
     #==========================================================================
     #Now one can search on augmented metadata...not the best use case example but...
     links6 = get_links(json_dict_augmented, 'file_size', int(FILE_SIZE_FAILED)) #...lists all failed attemps
-    print 'Failed to obtain, file sizes for the following links...will have to investigate...' 
-    print_json_dict(links6)
     #==========================================================================
-    print "Warning: this section downloads files into your current directory"
-    print " - you may want to simulate the download first but be advised that"
-    print "   empty files will be written to your current directory."
-    if True: #Example: of a specific save function 
+    # Warning: this section downloads files into your current directory
+    # you may want to simulate the download first but be advised that
+    # empty files will be written to your current directory.
+    if False: #Example: of a specific save function 
         save_csvs_by_dataset()  #...by default saves up to 5 csv files with dataset="Contracts Finder - All Notices"
         save_csvs_by_dataset("Contracts Finder - All Notices")  #...does the same as above
         save_csvs_by_dataset("Buyandsell.gc.ca - Tenders Data", 10, simulate==True) #... simulate saving up to 10 with dataset="Bsyandsell..."
@@ -390,17 +252,11 @@ def use_cases():
         links = get_links(json_dict, 'dataset', "Contract Finder - All notices" ) 
         links = get_links(json_dict, 'format', "CSV" ) 
     
-        save_files(links, simulate==True) #... links are urls and True means simulate.
-        
-    print "Finished use_cases"
+        save_files(links[0:5], simulate==True) #... links are urls and True means simulate.
     
     return
 #----------------------------------------------------------------------------
 
-#Future Features:
-# - add comments regarding what this function is about.
-# - Note: the above code can be put into a class maybe called OCDS_Api
-# - unit testing? 
 #==============================================================================
 
 #use_cases()
